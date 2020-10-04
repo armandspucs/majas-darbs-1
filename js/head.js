5f66f2150da4eb24cf76198cba371cd94a7888b3
Uldis Grunde-Zeiferts <uldis.grunde@gmail.com>
2020-10-04 13:21:37 +0300

    header /footer ģenerēšanas skripts

---
+++ js/head.js
@@ -0,0 +1,13 @@
+//head of document
+function head(title){
+	document.write("\t<div class=\"wrap\">\n");
+	document.write("\t\t<div class=\"header\">\n");
+	document.write("\t\t\t<img src=\"ing/logo.png\" \>\n");
+	document.write("\t\t\t<h2>Datortehnikas uzskaites sistēma</h2>\n");
+	document.write("\t\t\t<hr>\n");
+	document.write("\t</div>\n\n");
+	document.write("\t<div class=\"content\">\n");
+	document.write("\t\t<img class=\"back_btn\" src=\"img/back.png\">\n");
+	document.write("\t\tAtpakaļ\n");
+	document.write("\t\t<h4>"+title+"</h4>\n\n");
+}
