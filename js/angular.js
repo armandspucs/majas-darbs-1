4d33001f19184877e6813fe07bf3748b1b8fc606
Uldis Grunde-Zeiferts <uldis.grunde@gmail.com>
2020-10-10 11:56:25 +0300

    data=>dati

--- js/angular.js
+++ js/angular.js
@@ -1,6 +1,7 @@
 function Ctrl(dati){
 	var app = angular.module("myApp", []);
 	app.controller("myCtrl", function($scope) {
-		$scope.data = obj.dati;//JSON.parse(json);
+		$scope.dati = dati;//JSON.parse(json);
+		//alert(dati);
 	});
 }
