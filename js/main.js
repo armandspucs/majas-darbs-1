e393c7c58cbc93730c1833e487707e38d3e11394
armandspucs <53978384+armandspucs@users.noreply.github.com>
2020-11-07 21:06:14 +0200

    a

--- js/main.js
+++ js/main.js
@@ -16,22 +16,6 @@ function optionList(json, selectItem, objectId){
 
 
 
-///////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////
-// / _|   | |     | |      / _|           | |      (_|_)      
-//| |_ ___| |_ ___| |__   | |_ _   _ _ __ | | _____ _ _  __ _ 
-//|  _/ _ \ __/ __| '_ \  |  _| | | | '_ \| |/ / __| | |/ _` |
-//| ||  __/ || (__| | | | | | | |_| | | | |   < (__| | | (_| |
-//|_| \___|\__\___|_| |_| |_|  \__,_|_| |_|_|\_\___|_| |\__,_|
-//                                                  _/ |
-async function iegutDatusNoApi(url)
-{
-  let datiNoServera = await fetch(url);
-  let datiJson = await datiNoServera.json();
-  return datiJson;
-}
-//funkciju var izsaukt šādi
-//let datiJson = await iegutDatusNoApi('https://armandspucs.github.io/majas-darbs-1/data/datoruDB.json');
 
 
 //////////////////////////////////////////////////////////////
@@ -45,15 +29,28 @@ async function iegutDatusNoApi(url)
 //                        |_|                          |___/
 //paraugi tiek testēti failā fetch_test.html
 
-//ar zemāko funkciju var notestēt vai darbojas iepriekšējā funkcija iegutDatusNoApi()
-async function testIegutDatusNoApi()
-{
-  let datiJson = await iegutDatusNoApi('https://armandspucs.github.io/majas-darbs-1/data/datoruDB.json');
-
-  console.log(datiJson);
-}
-
 
 async function raditDatoruDB() //parāda datus tabula failā fetch_test.html
 {
+	let datiNoServera = await fetch('https://armandspucs.github.io/majas-darbs-1/data/datoruDB.json');
+	let datiJson = await datiNoServera.json();
+
+	let ierakstu_skaits = datiJson.dati.length; 
+	//ievērojiet ka visa info ir apakšobjektā 'dati' (tāda struktūra no excel nāk)
+	
+	tabulasRindas = document.querySelector('.tabulasRindas');
+
+	for (i = 0; i < ierakstu_skaits; i++)
+	{
+
+		tabulasRindas.innerHTML+=`
+		<tr>
+		<td> `+datiJson.dati[i]['id']+` </td>
+		<td> `+datiJson.dati[i]['razotajs']+` </td>
+		<td> `+datiJson.dati[i]['ram']+` </td>
+		</tr>`;
+		
+  
+	}//loop beigas
+
 }
