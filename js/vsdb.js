5883bd5ebfaa08d3aa9a09be15169d7596eae629
anrlic <70010867+anrlic@users.noreply.github.com>
2020-12-10 23:35:26 +0200

    gg
    gg

--- js/vsdb.js
+++ js/vsdb.js
@@ -1,3 +1,27 @@
+async function sikaakPeecNumura(a) //parāda datus tabula failā fetch_test.html
+{
+    let datiNoServera = await fetch('https://armandspucs.github.io/majas-darbs-1/data/datoruDB.json');
+    let datiJson = await datiNoServera.json();
+
+    let ierakstu_skaits = datiJson.dati.length;
+    //ievērojiet ka visa info ir apakšobjektā 'dati' (tāda struktūra no excel nāk)
+
+    tabulasRindas = document.querySelector('.tabulasRindas');
+    for (i = 0; i < ierakstu_skaits; i++) {
+
+       
+		if (datiJson.dati[i]['inventaraNr']==a){
+			
+        tabulasRindas.innerHTML += `
+		<br>
+		<br> ` + datiJson.dati[i]['inventaraNr'] + ` 
+		<br> ` + datiJson.dati[i]['iegadesGads'] + ` 
+		<br> ` + datiJson.dati[i]['piegadatajs'] + ` 
+		<br>`;
+		}
+    } //loop beigas
+
+}
 
 /*
 "id": "2",
@@ -30,9 +54,9 @@ async function raditVisasTehnikasDB() //parāda datus tabula failā fetch_test.h
 		<td> ` + datiJson.dati[i]['tips'] + ` </td>
 		<td> ` + datiJson.dati[i]['inventaraNr'] + ` </td>
 		<td> ` + datiJson.dati[i]['name'] + ` </td>
-		<th><input type="button" id="` + datiJson.dati[i]['inventaraNr']+ `" value="+" onclick="rad(this,'b');" /></th>
+		<th><input type="button" id="` + datiJson.dati[i]['inventaraNr']+ `" value="+" onclick="rad(this,'b');" /></th>`;
+		sikaakPeecNumura(datiJson.dati[i]['inventaraNr']);
 		<!--<td> sīkāka informācija </td> -->
-		</tr>`;
+		 tabulasRindas.innerHTML += `</tr>`;
     } //loop beigas
-
 }
